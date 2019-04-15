af2a458f91384d8f74748b3194f2a8b0a3612302
opus1269 <igreilly1269@gmail.com>
2019-04-14 22:40:15 -0500

    add dedupingMixin

--- app/elements/mixins/i8n_mixin.ts
+++ app/elements/mixins/i8n_mixin.ts
@@ -19,77 +19,20 @@ import '../../scripts/chrome-extension-utils/scripts/ex_handler.js';
  * @mixinFunction
  * @polymer
  */
-export const I8nMixin = (superClass: new () => PolymerElement) => class extends superClass {
+export const I8nMixin = dedupingMixin((superClass: new () => PolymerElement) => class extends superClass {
 
   constructor() {
     super();
   }
 
+  /**
+   * Localize a string
+   *
+   * @param name - name from _locales
+   * @param def - default value if name not found
+   */
   public localize(name: string, def: string = null) {
     return ChromeLocale.localize(name, def);
   }
 
-};
-
-
-/**
- * Element class mixin that provides API for adding Polymer's cross-platform
- * gesture events to nodes.
- *
- * The API is designed to be compatible with override points implemented
- * in `TemplateStamp` such that declarative event listeners in
- * templates will support gesture events when this mixin is applied along with
- * `TemplateStamp`.
- *
- * @mixinFunction
- * @polymer
- * @summary Element class mixin that provides API for adding Polymer's
- *   cross-platform
- * gesture events to nodes
- */
-// export const GestureEventListeners = dedupingMixin(
-//     /**
-//      * @template T
-//      * @param {function(new:T)} superClass Class to apply mixin to.
-//      * @return {function(new:T)} superClass with mixin applied.
-//      */
-//     (superClass) => {
-//       /**
-//        * @polymer
-//        * @mixinClass
-//        * @implements {Polymer_GestureEventListeners}
-//        */
-//       class GestureEventListeners extends superClass {
-//         /**
-//          * Add the event listener to the node if it is a gestures event.
-//          *
-//          * @param {!EventTarget} node Node to add event listener to
-//          * @param {string} eventName Name of event
-//          * @param {function(!Event):void} handler Listener function to add
-//          * @return {void}
-//          * @override
-//          */
-//         _addEventListenerToNode(node, eventName, handler) {
-//           if (!addListener(node, eventName, handler)) {
-//             super._addEventListenerToNode(node, eventName, handler);
-//           }
-//         }
-//
-//         /**
-//          * Remove the event listener to the node if it is a gestures event.
-//          *
-//          * @param {!EventTarget} node Node to remove event listener from
-//          * @param {string} eventName Name of event
-//          * @param {function(!Event):void} handler Listener function to remove
-//          * @return {void}
-//          * @override
-//          */
-//         _removeEventListenerFromNode(node, eventName, handler) {
-//           if (!removeListener(node, eventName, handler)) {
-//             super._removeEventListenerFromNode(node, eventName, handler);
-//           }
-//         }
-//       }
-//
-//       return GestureEventListeners;
-//     });
+});
